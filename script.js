7b6e10496972ee629424c955f8fff459bc12bb9c
lcleandrocosta <lcleandrocosta@gmail.com>
2022-03-30 09:14:41 -0300

    Implementar jumps do dinossauro

---
+++ script.js
@@ -0,0 +1,37 @@
+const dino = document.querySelector('.dino');
+let isJumping = false;
+
+function handleKeyup(event) {
+  if(event.keyCode === 32) {
+    if (!isJumping) {
+      jump();
+    } 
+  }
+}
+
+function jump() {
+  let position = 0;
+  isJumping = true;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      clearInterval(upInterval);
+
+      //Descendo
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20);
+    }else {
+      //Subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  },20);
+}
+document.addEventListener('keyup', handleKeyup);
