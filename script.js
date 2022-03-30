4ac99cbe67c83bc06eb76af10529a5de5a647b0a
lcleandrocosta <lcleandrocosta@gmail.com>
2022-03-30 10:49:47 -0300

    Alterar funcões cactus

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyup(event) {
   if(event.keyCode === 32) {
@@ -11,7 +12,6 @@ function handleKeyup(event) {
 }
 
 function jump() {
-  let position = 0;
   isJumping = true;
 
   let upInterval = setInterval(() => {
@@ -39,9 +39,27 @@ function jump() {
 function createCactus () {
   const cactus = document.createElement('div');
   let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
   cactus.classList.add('cactus');
   cactus.style.left = 1000 + 'px';
   background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+      // Game Over
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
 }
 
 createCactus();
